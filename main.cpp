9025b1642051bc222f9493b9763a76d076728248
msforenter090 <msforenter090@gmail.com>
2018-04-14 07:03:58 -0700

    Integrated spdlog into build system.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+#include "spdlog/spdlog.h"
+
 int main(int argc, char** argv) {
-    std::cout<<"Hello ATB."<<std::endl;
+    // ------------------------------------------------------------------------
+    // Test spdlog on console.
+    // ------------------------------------------------------------------------
+    auto console = spdlog::stdout_color_mt("console");
+
+    console->info("Welcome to spdlog!");
+    console->error("Some error message with arg{}..", 1);
+    
+    system("pause");
+    return 0;
 }
