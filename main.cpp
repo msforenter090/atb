7a84bd1b912b3cf5153b251ca610506f94f8742a
msforenter090 <msforenter090@gmail.com>
2018-04-15 09:47:23 -0700

    Added boost in cmake.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+// ----------------------------------------------------------------------------
+// Logging.
+// ----------------------------------------------------------------------------
 #include "spdlog/spdlog.h"
+
+// ----------------------------------------------------------------------------
+// Thread example.
+// ----------------------------------------------------------------------------
+#include <boost/thread.hpp>
+#include <boost/chrono.hpp>
+#include <iostream>
+
+void wait(int seconds) {
+    boost::this_thread::sleep_for(boost::chrono::seconds{ seconds });
+}
+
+void thread() {
+    for (int i = 0; i < 5; ++i) {
+        wait(1);
+        std::cout << i << '\n';
+    }
+}
 
 int main(int argc, char** argv) {
     // ------------------------------------------------------------------------
@@ -10,7 +31,13 @@ int main(int argc, char** argv) {
 
     console->info("Welcome to spdlog!");
     console->error("Some error message with arg{}..", 1);
-    
+
+    // ------------------------------------------------------------------------
+    // Test boost thread.
+    // ------------------------------------------------------------------------
+    boost::thread t{ thread };
+    t.join();
+
     system("pause");
     return 0;
 }
